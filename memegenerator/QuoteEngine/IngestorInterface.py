226e8896ddb1d07fcb9a7d4b7a2e3bcda579b073
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-23 17:44:35 +0200

    feat: init IngestorInterface

---
+++ memegenerator/QuoteEngine/IngestorInterface.py
@@ -0,0 +1,28 @@
+"""Ingestor Interface.
+
+An interface for ingesting quotes in different formats
+"""
+from abc import ABC, abstractmethod
+from typing import List
+from .quote import QuoteModel
+
+
+class IngestorInterface(ABC):
+    """Ingestor Interface.
+    An abstract class for parsing quotes in different format files
+    """
+
+    @classmethod
+    @abstractmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        """Parse a file and return a list of quotes
+        :param path: a fully qualified pathname to a file
+
+        Returns a List of quotes
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def can_ingest(cls, path) -> bool:
+        pass
