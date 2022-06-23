69fcfdc3ef82444e5f4a46f219f9139df905d479
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-23 17:56:30 +0200

    feat: complete QuoteModel with a str and repl method

--- memegenerator/QuoteEngine/QuoteModel.py
+++ memegenerator/QuoteEngine/QuoteModel.py
@@ -3,8 +3,16 @@
 
 class QuoteModel:
     """Quote Model.
-    A model for quotes."""
+    A model for building quotes."""
 
     def __init__(self, author, body):
         self.author = author
         self.body = body
+
+    def __repl__(self) -> str:
+        """Returns a human-readable quote with the author"""
+        return f"{self.body} - {self.author}"
+
+    def __str__(self) -> str:
+        """Returns a human-readable quote with the author"""
+        return self.__repl__()
